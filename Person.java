9124e12cf6621661e87af2b63533c4632e0f8321
Henning Ainödhofer <henning.ainoedhofer@mga.bistum365.de>
2026-02-25 13:12:44 +0100

    Initial sharing of project

---
+++ Person.java
@@ -0,0 +1,57 @@
+/**
+ *  Die abstrakte Klasse Person fasst alle Attribute und Methoden zusammen, die die beiden
+ *  erbenden Klassen Croupier und Spieler gemeinsam haben. Mit der abstrakten Methoden spielen()
+ *  wird auch gefordert, diese Methode in jeder Klasse zu überschreiben.
+ */
+public abstract class Person 
+{
+    /* Attribute */
+    protected Wuerfel wuerfel;
+    private int aktErgebnis;
+    /*Konstruktor*/
+    
+    /**
+     * Im Konstruktor wird dem Attribut Wuerfel ein neues Objekt vom Typ Würfel zugewiesen.
+     */
+    public Person()
+    {
+        wuerfel = new Wuerfel();
+    }
+    /* Methoden */
+    
+    
+    public void setzeAktErgebnis(int  pAktErgebnis){
+        
+    }
+
+    /**
+     * Diese Methode setzt die Spiellogik im Spielerobjekt um
+     */
+    public abstract void spielen();
+
+    
+    public int addiere(int  zahl1,int  zahl2){
+        
+    }
+
+    
+    public int gibAktErgebnis(){
+        
+    }
+
+    /**
+     * Diese Methode hält den Prozessor für 1 Sekunde lang an, bevor der Prozessor weiter rechnet.
+     */
+    public void warte()
+    {
+        try
+        {
+            Thread.sleep(1000);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+}//Ende Klasse: Person
+
